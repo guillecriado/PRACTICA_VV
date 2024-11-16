4dcf218e0b6ffa7a4ed1445ed85b6367c212c6f8
Andres Ospina <andres.ospina@alumnos.upm.es>
2024-11-17 00:44:05 +0100

    Se vuelven a subir las pruebas de caja blanca para alien y bomb

--- Space-Invaders-Practica/src/test/java/space_invaders/alien/AlienTest_CajaBlanca.java
+++ Space-Invaders-Practica/src/test/java/space_invaders/alien/AlienTest_CajaBlanca.java
@@ -5,94 +5,89 @@ import org.junit.jupiter.api.*;
 import space_invaders.sprites.Alien;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class AlienTestCajaBlanca {
+public class AlienTest_CajaBlanca {
     Alien alien;
     Alien.Bomb bomb;
+    @Test
+    void alienCajaBlancaCP1(){
+        alien = new Alien(164,175);
+        int posicionXEsperada = (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) / 2 ;
+        int posicionYEsperada = Commons.BOARD_HEIGHT / 2;
 
-    @Nested
-    public class alienConstructorTestCajaBlanca {
-        @Test
-        void alienCajaBlancaCP1(){
-            alien = new Alien(164,175);
-            int posicionXEsperada = (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) / 2 ;
-            int posicionYEsperada = Commons.BOARD_HEIGHT / 2;
-
-            assertAll("Caso de Prueba CP25: Posición de creación del alien",
-                    () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 164"),
-                    () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 175"));
-        }
-
-        @Test
-        void alienCajaBlancaCP2(){
-            alien = new Alien(359, 175);
-            int posicionXEsperada = Commons.BOARD_WIDTH - Commons.BORDER_RIGHT ;
-            int posicionYEsperada = Commons.BOARD_HEIGHT / 2;
-
-            assertAll("Caso de Prueba CP46: Posición de creación del alien",
-                    () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 328"),
-                    () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 175"));
-        }
-
-        @Test
-        void alienCajaBlancaCP3(){
-            alien = new Alien(-1, 175);
-            int posicionXEsperada = Commons.BORDER_LEFT;
-            int posicionYEsperada = Commons.BOARD_HEIGHT / 2;
-
-            assertAll("Caso de Prueba CP4: Posición de creación del alien",
-                    () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 5"),
-                    () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 175"));
-        }
-
-        @Test
-        void alienCajaBlancaCP4(){
-            alien = new Alien(164, 351);
-            int posicionXEsperada = (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) / 2 ;
-            int posicionYEsperada = Commons.BOARD_HEIGHT ;
-
-            assertAll("Caso de Prueba CP288: Posición de creación del alien",
-                    () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 164"),
-                    () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 350"));
-        }
-
-        @Test
-        void alienCajaBlancaCP5(){
-            alien = new Alien(164, -1);
-            int posicionXEsperada = 179 ;
-            int posicionYEsperada = 0;
-
-            assertAll("Caso de Prueba CP22: Posición de creación del alien",
-                    () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 164"),
-                    () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 0"));
-        }
+        assertAll("Caso de Prueba CP25: Posición de creación del alien",
+                () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 164"),
+                () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 175"));
     }
 
-    @Nested
-    class alienBombTestCajaBlanca {
+    @Test
+    void alienCajaBlancaCP2(){
+        alien = new Alien(359, 175);
+        int posicionXEsperada = Commons.BOARD_WIDTH - Commons.BORDER_RIGHT ;
+        int posicionYEsperada = Commons.BOARD_HEIGHT / 2;
 
-        @Test
-        void alienBombCajaBalncaCP1() {
-            alien = new Alien(164, 175);
-            bomb = alien.getBomb();
-            int posicionBombXEsperada = (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) / 2 ;
-            int posicionBombYEsperada = Commons.BOARD_HEIGHT / 2;
+        assertAll("Caso de Prueba CP46: Posición de creación del alien",
+                () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 328"),
+                () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 175"));
+    }
 
-            assertAll("Caso de Prueba CP25: Posición de creación de Bomb",
-                    () -> assertEquals(posicionBombXEsperada, bomb.getX(), "La coordenada X debería ser 164"),
-                    () -> assertEquals(posicionBombYEsperada, bomb.getY(), "La coordenada Y debería ser 175"));
-        }
+    @Test
+    void alienCajaBlancaCP3(){
+        alien = new Alien(-1, 175);
+        int posicionXEsperada = Commons.BORDER_LEFT;
+        int posicionYEsperada = Commons.BOARD_HEIGHT / 2;
 
-        @Test
-        void alienBombCajaBlancaCP2(){
-            alien = new Alien(359, 351);
-            bomb = alien.getBomb();
-            int posicionBombXEsperada = Commons.BOARD_WIDTH - Commons.BORDER_RIGHT ;
-            int posicionBombYEsperada = Commons.BOARD_HEIGHT ;
+        assertAll("Caso de Prueba CP4: Posición de creación del alien",
+                () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 5"),
+                () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 175"));
+    }
 
-            assertAll("Caso de Prueba CP2: Posición de creación de Bomb",
-                    () -> assertEquals(posicionBombXEsperada, bomb.getX(), "La coordenada X debería ser 328"),
-                    () -> assertEquals(posicionBombYEsperada, bomb.getY(), "La coordenada Y debería ser 350"));
-        }
+    @Test
+    void alienCajaBlancaCP4(){
+        alien = new Alien(164, 351);
+        int posicionXEsperada = (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) / 2 ;
+        int posicionYEsperada = Commons.BOARD_HEIGHT ;
+
+        assertAll("Caso de Prueba CP288: Posición de creación del alien",
+                () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 164"),
+                () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 350"));
+    }
+
+    @Test
+    void alienCajaBlancaCP5(){
+        alien = new Alien(164, -1);
+        int posicionXEsperada = 179 ;
+        int posicionYEsperada = 0;
+
+        assertAll("Caso de Prueba CP22: Posición de creación del alien",
+                () -> assertEquals(posicionXEsperada, alien.getX(), "La coordenada X debería ser 164"),
+                () -> assertEquals(posicionYEsperada, alien.getY(), "La coordenada Y debería ser 0"));
+    }
+
+
+
+    @Test
+    void alienBombCajaBalncaCP1() {
+        alien = new Alien(164, 175);
+        bomb = alien.getBomb();
+        int posicionBombXEsperada = (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) / 2 ;
+        int posicionBombYEsperada = Commons.BOARD_HEIGHT / 2;
+
+        assertAll("Caso de Prueba CP25: Posición de creación de Bomb",
+                () -> assertEquals(posicionBombXEsperada, bomb.getX(), "La coordenada X debería ser 164"),
+                () -> assertEquals(posicionBombYEsperada, bomb.getY(), "La coordenada Y debería ser 175"));
+    }
+
+    @Test
+    void alienBombCajaBlancaCP2(){
+        alien = new Alien(359, 351);
+        bomb = alien.getBomb();
+        int posicionBombXEsperada = Commons.BOARD_WIDTH - Commons.BORDER_RIGHT ;
+        int posicionBombYEsperada = Commons.BOARD_HEIGHT ;
+
+        assertAll("Caso de Prueba CP2: Posición de creación de Bomb",
+                () -> assertEquals(posicionBombXEsperada, bomb.getX(), "La coordenada X debería ser 328"),
+                () -> assertEquals(posicionBombYEsperada, bomb.getY(), "La coordenada Y debería ser 350"));
     }
 }
